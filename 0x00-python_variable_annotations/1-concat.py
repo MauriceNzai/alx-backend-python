a1aac7e4190c6aeea9f53f9f75a9281600366d1d
Maurice Nzai <maurice.nzai@gmail.com>
2024-03-14 15:32:58 +0300

    Add Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+type-annotated function
+"""
+def concat(str1: str, str2: str)-> str:
+    """
+    returns concatenated string str1 and str2
+    """
+    res: str = str1 + str2
+    return res
