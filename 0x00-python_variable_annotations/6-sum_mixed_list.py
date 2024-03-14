17fb3a12d4cec8d03b8a0fb4da8f29d53891649c
Maurice Nzai <maurice.nzai@gmail.com>
2024-03-14 15:38:24 +0300

    Add Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+type-annotated function to sum list of mixed values
+"""
+
+from typing import List, Union
+def sum_mixed_list(mxd_list: List[Union[float, int]])-> float:
+    """
+    returns sum of list elements
+    """
+    res: float = sum(mxd_list)
+    return res
