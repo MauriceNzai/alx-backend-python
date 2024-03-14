28bc69bb1a430537067bb3bf69dae5f756b920f6
Maurice Nzai <maurice.nzai@gmail.com>
2024-03-14 15:40:42 +0300

    Add Task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Type annotations
+"""
+from typing import Iterable, List, Sequence, Tuple, Union 
+
+def element_length(lst: Iterable[Sequence])-> List[Tuple[Sequence, int]]:
+    """
+    type annotated function
+    """
+    return [(i, len(i)) for i in lst]
