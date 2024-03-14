4b63503f7818fea986e5c0638b5e219759f2703b
Maurice Nzai <maurice.nzai@gmail.com>
2024-03-14 17:01:43 +0300

    Check Style

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,9 +4,11 @@
 type-annotated function to return a function
 """
 
+
 from typing import Callable
 
-def make_multiplier(multiplier: float)-> Callable[[float], float]:
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     returns a function that multiplies a float by multiplier
     """
