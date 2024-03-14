12736ff3f413474d3360b930459837ae4c0e6d57
Maurice Nzai <maurice.nzai@gmail.com>
2024-03-14 15:34:37 +0300

    Add Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+type-annotated function
+"""
+import math
+
+def floor(n: float)-> int:
+    """
+    returns floor of a float
+    """
+    res: int = math.floor(n)
+    return res
