d391922051c9f19d26841592ce813fe5107b8252
Maurice Nzai <maurice.nzai@gmail.com>
2024-03-14 15:35:54 +0300

    Add Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+type-annotated function
+"""
+
+def to_str(n: float)-> str:
+    """
+    returns a string from a float value
+    """
+    res: str = str(n)
+    return res
