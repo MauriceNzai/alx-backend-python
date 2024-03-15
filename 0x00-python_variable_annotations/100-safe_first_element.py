dc50c6896730a1985169022dd0d7d1d2ac5d20c0
Maurice Nzai <maurice.nzai@gmail.com>
2024-03-15 08:04:20 +0300

    Update 100-safe_first_element.py
    Added function documentation

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -9,6 +9,9 @@ from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    correct duck-typed annotations added
+    """
     if lst:
         return lst[0]
     else:
