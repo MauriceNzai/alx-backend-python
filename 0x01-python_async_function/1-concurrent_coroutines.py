7bc6a48d710c7cbc3dfc622aff38c788998b55da
Maurice <maurice.nzai@gmail.com>
2024-03-18 15:47:51 +0000

    Rename file

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+''' Description: Import wait_random from the previous python file that
+                 written
+
+                 wait_n returns the list of all the delays(float values)
+                 in ascending order without using sort() because of concurrency.
+    Arguments: n: int, max_delay: int = 10
+'''
+
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Waits for ran delay until max_delay, returns list of actual delays """
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
