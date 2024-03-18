0e26aafa70030d313a914c9a55fc18d9e9e8f323
Maurice Nzai <maurice.nzai@gmail.com>
2024-03-19 02:21:14 +0300

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -16,3 +16,11 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     spawn_ls = []
     delay_ls = []
     for i in range(n):
+      delayed_task = asyncio.create_task(wait_random(max_delay)) 
+      delayed_task.add_done_callback(lambda x: delay_ls.append(x.result())) 
+      spawn_ls.append(delayed_task) 
+  
+     for spawn in spawn_ls: 
+         await spawn 
+  
+     return delay_ls
