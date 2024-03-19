2c87c5e4f74e3bce74f782d7d11c5bba31203fd1
Maurice <maurice.nzai@gmail.com>
2024-03-19 18:19:22 +0000

    Update Task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -16,6 +16,11 @@ async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     spawn_ls = []
     delay_ls = []
     for i in range(n):
-        delayed_task = task_wait_random(max_delay)
-        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
-        spawn_ls.append(delayed_task)
+        ed_task = task_wait_random(max_delay)
+        ed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(ed_task)
+
+    for spawn in spawn_ls:
+        await spawn
+
+    return delay_ls
