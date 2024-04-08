98503d499afb086d3a869bf4d593071463809754
Maurice <maurice.nzai@gmail.com>
2024-04-08 12:28:20 +0000

    Add Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -47,3 +47,14 @@ class TestGithubOrgClient(unittest.TestCase):
         x = GithubOrgClient(org)
         self.assertEqual(x.org, expected)
         get_patch.assert_called_once_with("https://api.github.com/orgs/"+org)
+
+    def test_public_repos_url(self):
+        """
+        test that _public_repos_url works
+        """
+        expected = "www.yes.com"
+        payload = {"repos_url": expected}
+        to_mock = 'client.GithubOrgClient.org'
+        with patch(to_mock, PropertyMock(return_value=payload)):
+            client = GithubOrgClient("x")
+            self.assertEqual(cli._public_repos_url, expected)
